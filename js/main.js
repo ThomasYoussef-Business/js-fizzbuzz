ff3a7d29c2c34b4677102b2a53d3cb3a6f0bcdd0
ThomasYoussef-Business <thomasyoussef.business@protonmail.com>
2022-11-11 17:35:03 +0100

    Aggiunto CSS per le scatolette

--- js/main.js
+++ js/main.js
@@ -1,16 +1,43 @@
+import {
+    ensureFloatFromPrompt,
+    ensureIntFromPrompt
+} from "./ensureNumber.js";
 "use strict";
-let numeroDiPartenza = 1, numeroFinale = 10_000, risultatoParole = "";
-let paroleEDivisori = {
-    Fizz: 3,
-    Buzz: 5,
-}
+console.log(ensureFloatFromPrompt)
 
+let numeroDiPartenza,
+    numeroFinale,
+    risultatoParole = "";
+
+let paroleEDivisori = {};
+paroleEDivisori[prompt("Metti la prima parola: ")] = ensureIntFromPrompt(
+    "Inserisci il primo numero: ",
+    "Questo non è un numero! Inserisci un numero valido: ",
+    (x) => x <= 0
+);
+
+paroleEDivisori[prompt("Metti la seconda parola: ")] = ensureIntFromPrompt(
+    "Inserisci il secondo numero: ",
+    "Questo non è un numero! Inserisci un numero valido: ",
+    (x) => x <= 0
+);
+
+numeroDiPartenza = ensureIntFromPrompt("Inserisci il numero di partenza: ",
+    "Questo numero non va bene! Riprova: ",
+    (x) => x <= 0);
+
+numeroFinale = ensureIntFromPrompt("Inserisci il numero finale: ",
+    "Questo numero non va bene! Riprova: ",
+    (x) => x > 3000 || x <= 0);
+
+let contenitoreBlocchi = document.getElementById("container-scatole")
 for (let i = numeroDiPartenza; i < numeroFinale + 1; i++) {
     risultatoParole = "";
     for (let word of Object.keys(paroleEDivisori)) {
         if (i % paroleEDivisori[word] == 0) {
-            risultatoParole += word
+            risultatoParole += word;
         }
     }
-    console.log(risultatoParole.length == 0 ? i : risultatoParole)
+    risultatoParole = risultatoParole == "" ? i.toString() : risultatoParole;
+    contenitoreBlocchi.innerHTML += `<div class="m-3">${risultatoParole}</div>`;
 }
